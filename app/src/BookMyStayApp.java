23f8e761436b0948b8fb178964f9803f5b8757d0
shaiksahid28 <directorsahidbabu@gmail.com>
2026-03-11 15:01:41 +0530

    UC1

---
+++ app/src/BookMyStayApp.java
@@ -0,0 +1,16 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("        BOOK MY STAY APPLICATION      ");
+        System.out.println("      Hotel Booking System v1.0       ");
+        System.out.println("======================================");
+        System.out.println("Welcome to the Hotel Booking System.");
+        System.out.println("System started successfully.");
+        System.out.println("Thank you for using Book My Stay.");
+        System.out.println("======================================");
+
+    }
+}
+
